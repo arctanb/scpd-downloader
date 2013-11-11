6bd63c366fa30428b21120526bce8f7d28db162f
Bryant Tan <arctanb@gmail.com>
2013-11-11 11:32:39 -0800

    now it works

--- download.py
+++ download.py
@@ -3,14 +3,18 @@ import sys
 
 if len(sys.argv) != 2:
   print 'usage: %s [filename]' % sys.argv[0]
+  exit(0)
 
 lines = []
 
 with open(sys.argv[1]) as fin:
-  lines = fin.readlines()
+  lines = [l.strip() for l in fin.readlines()]
   fin.close()
 
 for url in lines:
   spl = url.split('/')
-  fname = '%s-%s.wmv' % (spl[4], spl[6].split('-')[0])
-  os.system('mimms -c %s %s' % (url, fname))
+  fname = '"%s-%s.wmv"' % (spl[4], spl[6].split('-')[0])
+  os.system('mimms -c %s tmp' % url)
+  os.system('mv tmp %s' % fname)
+
+print 'done'
